a1162ac50d73774062d2647d71aa1a88a7e67fdf
Rakibul Hoda Labib <154999586+labib10849@users.noreply.github.com>
2024-11-07 02:20:37 +0600

    app.py

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ diabetes_model = pickle.load(open(f'{working_dir}/saved_models/diabetes_model.sa
 with st.sidebar:
     selected = option_menu('Multiple Disease Prediction System',
                            ['Diabetes Prediction'],
-                           menu_icon='hospital-fill',
+                           menu_icon='hospital-user',
                            icons=['activity'],
                            default_index=0)
 
